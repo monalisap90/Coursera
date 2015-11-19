b0e3554272a2f672e20319da4c2fb4a9fb5350b4
monalisap90 <monalisap90@gmail.com>
2015-11-19 15:12:52 +0530

    Update cachematrix.R
    Submitted by Monalisa Pal

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,33 @@
-
-makeCacheMatrix <- function(x = matrix()) {
-  m<-NULL
-  set<-function(y){
-  x<<-y
-  m<<-NULL
-}
-get<-function() x
-setmatrix<-function(solve) m<<- solve
-getmatrix<-function() m
-list(set=set, get=get,
-   setmatrix=setmatrix,
-   getmatrix=getmatrix)
-}
-
-cacheSolve <- function(x=matrix(), ...) {
-    m<-x$getmatrix()
-    if(!is.null(m)){
-      message("getting cached data")
-      return(m)
+makeCacheMatrix <- function(x = matrix())
+{
+    # This function creates a special "matrix", which is really
+    # a list containing four functions F1 through F4
+    i <- NULL
+    set <- function(y)  # F1: to set the value of the matrix
+    {
+        x <<- y
+        i <<- NULL  # initializes inverse to NULL while caching the matrix
     }
-    datos<-x$get()
-    m<-solve(datos, ...)
-    x$setmatrix(m)
-    m
+    get <- function() x # F2: to get the cached value of the matrix
+    setinv <- function(solve) i <<- solve # F3: to set the value of the inverse
+    getinv <- function() i # F4: to get the cached value of the inverse
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    # to return the list of four functions (F1 through F4)
+}
+
+cacheSolve <- function(x, ...)
+{
+    # This funcion calculates the inverse of a special "matrix"
+    # created with the function makeCacheMatrix()
+    i <- x$getinv() # fetches the cached value of matrix inverse
+    if(!is.null(i)) 
+    {
+        # if the fetched value is non-null => inverse was calculated before
+        message("getting cached matrix inverse")
+        return(i) # return the cached value of inverse
+    }
+    mat <- x$get() # if inverse is null, fetch the cached matrix 
+    i <- solve(mat, ...) # evaluate the inverse of the cached matrix
+    x$setinv(i) # cache the matrix inverse for future use
+    i # return the matrix inverse
 }
